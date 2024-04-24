a63559bd4ba3d68352985e65ab2048c897d58763
Gal Trodel <galtrudel@gmail.com>
2024-04-24 14:58:26 +0300

    check permissions

--- ex_1/routes/ratings.ts
+++ ex_1/routes/ratings.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  res.send("hello yossi ");
+  res.send("permission to push");
 });
 
 router.get("/:id", async (req: Request, res: Response) => {
